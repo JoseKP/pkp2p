c3e45fca174a8636a62b9f557a3a96c9088825e6
JoseKP <0x7dfe0bf47a66bdd5f50c8a35700be613e01fe4d0@skiff.com>
2023-06-11 10:39:55 +0000

    update

--- auto-push.js
+++ auto-push.js
@@ -1,28 +1,51 @@
-const chokidar = require('chokidar');
-const { exec } = require('child_process');
+const chokidar = require("chokidar");
+const { exec } = require("child_process");
 
 // Initialize watcher.
-const watcher = chokidar.watch('.', {
+const watcher = chokidar.watch(".", {
   ignored: /(^|[\/\\])\../, // ignore dotfiles
-  persistent: true
+  persistent: true,
 });
 
 // Something to use when events are received.
 const log = console.log.bind(console);
 
 // Add event listeners.
-watcher
-  .on('change', path => {
-    log(`File ${path} has been changed`);
-    exec('git add . && git commit -m "update" && git push', (err, stdout, stderr) => {
-      if (err) {
-        // Git commands failed
-        console.error(`exec error: ${err}`);
-        return;
-      }
-  
-      // the *entire* stdout and stderr (buffered)
-      log(`stdout: ${stdout}`);
-      log(`stderr: ${stderr}`);
-    });
+watcher.on("change", (path) => {
+  log(`File ${path} has been changed`);
+
+  exec("git diff --shortstat", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    // stdout will have a format like: " 1 file changed, 1 insertion(+), 1 deletion(-)"
+    const changes = stdout.split(",").map((str) => parseInt(str));
+    const filesChanged = changes[0];
+    const insertions = changes[1];
+    const deletions = changes[2];
+
+    // Now you can perform some logic based on the number of changes
+    if (filesChanged > 5 || insertions + deletions > 10) {
+      console.log("Significant changes detected");
+
+      exec(
+        'git add . && git commit -m "update" && git push',
+        (err, stdout, stderr) => {
+          if (err) {
+            // Git commands failed
+            console.error(`exec error: ${err}`);
+            return;
+          }
+
+          // the *entire* stdout and stderr (buffered)
+          log(`stdout: ${stdout}`);
+          log(`stderr: ${stderr}`);
+        }
+      );
+    } else {
+      console.log("No significant changes detected");
+    }
   });
+});
