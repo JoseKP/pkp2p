f1595a5a828451895a596f185ed42e4e40ff5fc1
JoseKP <0x7dfe0bf47a66bdd5f50c8a35700be613e01fe4d0@skiff.com>
2023-06-11 14:09:51 +0000

    update

--- apps/browser-peer/components/Loading.tsx
+++ apps/browser-peer/components/Loading.tsx
@@ -3,27 +3,13 @@ import { Alert, Space, Spin } from "antd";
 export const Loading = () => {
   return (
     <>
-      <Space direction="vertical" style={{ width: "100%" }}>
-        <Space>
-          <Spin tip="Loading" size="small">
-            <div className="content" />
-          </Spin>
-          <Spin tip="Loading">
-            <div className="content" />
-          </Spin>
-          <Spin tip="Loading" size="large">
-            <div className="content" />
-          </Spin>
-        </Space>
-
-        <Spin tip="Loading...">
-          <Alert
-            message="Alert message title"
-            description="Further details about the context of this alert."
-            type="info"
-          />
-        </Spin>
-      </Space>
+      <Spin tip="Loading...">
+        <Alert
+          message="Alert message title"
+          description="Further details about the context of this alert."
+          type="info"
+        />
+      </Spin>
     </>
   );
 };
