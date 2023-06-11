5ff41ca29c049d79c2121d2d5d3b654c22d5f391
JoseKP <0x7dfe0bf47a66bdd5f50c8a35700be613e01fe4d0@skiff.com>
2023-06-11 14:16:16 +0000

    update

--- apps/browser-peer/app/page.tsx
+++ apps/browser-peer/app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
-import { Button, Typography } from "antd";
+import { Button, Input, Space, Typography } from "antd";
 import { Wrapper } from "./Wrapper";
 import { Loading } from "../components/Loading";
+import Form from "../components/Form";
 
 export default function Page() {
   return (
     <Wrapper>
       <Typography.Title>PKP2P - BROWSER PEER</Typography.Title>
-      <Loading tip="UES'/>
+      <Space>
+        <Input placeholder="input placeholder" />
+      </Space>
+      <Space>
+        <Button type="primary">Button</Button>
+      </Space>
+      <Loading />
     </Wrapper>
   );
 }
